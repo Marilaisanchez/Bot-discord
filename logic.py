f59c88468bc85dc4efd35f85b43d93cbab3407ec
Marilaisanchez <marilaisanchez28gmail.com>
2025-01-12 20:22:57 -0600

    Cambios realizados

--- logic.py
+++ logic.py
@@ -1,4 +1,4 @@
-import random as r, string as s
+import random as r, string as s, discord
 
 def contra(largo):
     elements = s.ascii_letters+s.ascii_lowercase+s.ascii_uppercase+s.digits+s.punctuation
@@ -17,3 +17,46 @@ def flip_coin():
         return "HEADS"
     else:
         return "TAILS"
+    
+def add_numbers(left: int, right: int):
+    return f"{left + right}"
+
+def choose(*choices):
+    return r.choice(choices)
+
+def repeat_message(times, content='repeating...'):
+    return [content] * times
+
+def get_joined_date(member: discord.Member):
+    return discord.utils.format_dt(member.joined_at)
+
+def check_if_cool(ctx):
+    if ctx.invoked_subcommand is None:
+        return f'No, {ctx.subcommand_passed} is not cool'
+    else:
+        return None
+    
+def is_bot_cool():
+    return 'Yes, the bot is cool.'
+
+def roll(dice: str):
+    try:
+        num_dice, sides = map(int, dice.lower().split('d'))
+        results = [r.randint(1, sides) for _ in range(num_dice)]
+        total = sum(results)
+        return f"Tiradas: {results} | Total: {total}"
+    except ValueError:
+        return "Format has to be in NdN! Example: 2d6"
+
+def guess_logic(ctx, number: int):
+    value = r.randint(1, 6)
+    correct = (number == value)
+    return tick_logic(ctx, correct)
+
+def tick_logic(ctx, correct: bool):
+    emoji = '\N{WHITE HEAVY CHECK MARK}' if correct else '\N{CROSS MARK}'
+    try:
+        return ctx.message.add_reaction(emoji)
+    except discord.HTTPException:
+        return None
+
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import discord, os, logic as l
+import discord, os, logic as l, random as r
 from dotenv import load_dotenv
 from discord.ext import commands
 
@@ -10,29 +10,89 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='*', intents=intents)
 
+#Shows in the console that the bot has logged in Discord 
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
 
+#El bot se presenta (*hello)
 @bot.command()
 async def hello(ctx):
+    """El bot se presenta (*hello)"""
     await ctx.send(f'Hola, soy un bot {bot.user}!')
 
+#Repeats heh x amount of times (*heh 10)
 @bot.command()
 async def heh(ctx, count_heh = 5):
+    """Repeats heh x amount of times (*heh 10)"""
     await ctx.send("he" * count_heh)
 
+#Generates a psw with x amount of items (*psw 20)
 @bot.command(name = "psw")
-async def password(ctx, lenght=25):
-    a = l.contra(lenght)
+async def password(ctx, length=25):
+    """Generates a psw with x amount of items (*psw 20)"""
+    a = l.contra(length)
     await ctx.send(f"🔏Su contraseña será generada: {a}")
 
-@bot.command(name="coin")
+#Flips a coin/chooses randomly "TAILS" OR "HEADS" (*coin)
+@bot.command(name = "coin")
 async def luck(ctx):
+    """Flips a coin/chooses randomly "TAILS" OR "HEADS" (*coin)"""
     await ctx.send(l.flip_coin())
 
-@bot.command(name="emoji")
-async def caras(ctx):
-    await ctx.send(l.emoji())
+#Adds numbers (*add 2 4)
+@bot.command(name = "add")
+async def add_numbers(ctx, left: int, right: int):
+    """Adds numbers (*add 2 4)"""
+    await ctx.send(left + right)
+
+#Chooses one chooice in a list (*choose mom dad sis or 1 2 3)
+@bot.command(name = "choose")
+async def choose(ctx, *choices: str):
+    """Chooses one chooice in a list (*choose mom dad sis)"""
+    await ctx.send(r.choice(choices))
+
+#Repeats a frase or # x times (*repeat 3 Holaa, cómo estás?)
+@bot.command(name = "repeat")
+async def repeat(ctx, times: int, *, content='repeating...'):
+    """Repeats a frase or number x times (*repeat 3 Holaa, cómo estás?)"""
+    messages = l.repeat_message(times, content)
+    for message in messages:
+        await ctx.send(message) 
+
+#Writes the date and hour a member joined (*joined @Juan)
+@bot.command(name = "joined")
+async def joined(ctx, member: discord.Member):
+    """Writes the date and hour a member joined (*joined @Juan)"""
+    await ctx.send(f'{member.name} se unió el {discord.utils.format_dt(member.joined_at)}')
+
+#Checks if a subcommand has been invoked or not (*cool __ )
+@bot.group(name = "cool")
+async def cool(ctx):
+    """Checks if a subcommand has been invoked or not (*cool __ )"""
+    if ctx.invoked_subcommand is None:
+        await ctx.send(f'No, {ctx.subcommand_passed} is not cool')
+
+#Says the bot is cool (*cool bot)
+@cool.command(name = 'bot')
+async def is_bot_cool(ctx):
+    """Says the bot is cool (*cool bot)"""
+    await ctx.send('Yes, the bot is cool.')
+
+#Throws x amount of dice and adds the results
+@bot.command(name="roll")
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        result = l.roll(dice) 
+        await ctx.send(f'{result}')  
+    except Exception as e:
+        await ctx.send(f"Error: {e}")
+
+#Guess a random number from 1 to 6.
+@bot.command(name = "guess")
+async def guess(ctx, number: int):
+    """Guess a random number from 1 to 6."""
+    await l.guess_logic(ctx, number)
 
 bot.run(token)
